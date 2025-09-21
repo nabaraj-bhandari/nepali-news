ee051d13d1223b87e762cfbaf609d1e68e2ae378
nabaraj-bhandari <nabarajbhandari2005@gmail.com>
2025-09-22 01:01:15 +0545

    trying headers

--- utils/scraper.ts
+++ utils/scraper.ts
@@ -3,7 +3,16 @@ import * as cheerio from "cheerio";
 
 export async function scrapeEkantipur() {
   try {
-    const { data } = await axios.get("https://ekantipur.com/headlines");
+    const { data } = await axios.get("https://ekantipur.com/headlines", {
+      headers: {
+        "User-Agent":
+          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36",
+        "Accept-Language": "en-US,en;q=0.9",
+        Accept:
+          "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
+        Connection: "keep-alive",
+      },
+    });
     const $ = cheerio.load(data);
 
     return $("article > .teaser")
@@ -20,7 +29,7 @@ export async function scrapeEkantipur() {
       })
       .get();
   } catch (err: any) {
-    console.error("Scraper Error: Ekantipur", err.message || err);
-    throw new Error("Failed to scrape Ekantipur");
+    console.error("Scraper Error: eKantipur", err.message || err);
+    throw new Error("Failed to scrape eKantipur");
   }
 }
