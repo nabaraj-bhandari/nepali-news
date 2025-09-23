4f8a7d812dcc80f3a55fa3489d676298820afe57
nabaraj-bhandari <nabarajbhandari2005@gmail.com>
2025-09-24 01:07:56 +0545

    added more sites

--- config/sources.ts
+++ config/sources.ts
@@ -1,42 +1,6 @@
 import { WebScraperConfig } from "@/types/types";
 
 export const SITE_CONFIGS: WebScraperConfig[] = [
-  // Web
-  {
-    type: "web",
-    name: "The Kathmandu Post",
-    url: "https://kathmandupost.com/national",
-    baseUrl: "https://kathmandupost.com",
-    listSelector: "div > article",
-    titleSelector: "a > h3",
-    linkSelector: "a",
-  },
-  {
-    type: "web",
-    name: "Nagarik Dainik",
-    url: "https://nagariknews.nagariknetwork.com/main-news",
-    baseUrl: "https://nagariknews.nagariknetwork.com",
-    listSelector: "article > .text",
-    titleSelector: "h1 > a",
-    linkSelector: "h1 > a",
-  },
-  {
-    type: "web",
-    name: "eKantipur",
-    url: "https://ekantipur.com",
-    listSelector: "div > article",
-    titleSelector: "h2 > a,h1 > a",
-    linkSelector: "h2 > a,h1 > a",
-  },
-  {
-    type: "web",
-    name: "Gorkapatra",
-    url: "https://gorkhapatraonline.com/categories/national",
-    listSelector: "section > div > div > div > div > div > div > div",
-    titleSelector: "div > h2 > a",
-    linkSelector: "div > h2 > a",
-  },
-
   // RSS
   {
     type: "rss",
@@ -71,11 +35,80 @@ export const SITE_CONFIGS: WebScraperConfig[] = [
   {
     type: "rss",
     name: "Artha Sarokar",
-    url: "https://www.arthasarokar.com/feed",
+    url: "https://arthasarokar.com/feed",
   },
   {
     type: "rss",
     name: "Nepal Samaya",
-    url: "https://www.nepalsamaya.com/feed",
+    url: "https://nepalsamaya.com/feed",
+  },
+  {
+    type: "rss",
+    name: "Nepali Patra",
+    url: "https://www.nepalipatra.com/feed",
+  },
+  {
+    type: "rss",
+    name: "१२खरी",
+    url: "https://baahrakhari.com/feed",
+  },
+  {
+    type: "rss",
+    name: "लोकान्तर",
+    url: "https://lokaantar.com/feed",
+  },
+
+  // Web
+  {
+    type: "web",
+    name: "The Kathmandu Post",
+    url: "https://kathmandupost.com/national",
+    baseUrl: "https://kathmandupost.com",
+    listSelector: "div > article",
+    titleSelector: "a > h3",
+    linkSelector: "a",
+  },
+  {
+    type: "web",
+    name: "Nagarik Dainik",
+    url: "https://nagariknews.nagariknetwork.com/main-news",
+    baseUrl: "https://nagariknews.nagariknetwork.com",
+    listSelector: "article > .text",
+    titleSelector: "h1 > a",
+    linkSelector: "h1 > a",
+  },
+  {
+    type: "web",
+    name: "eKantipur",
+    url: "https://ekantipur.com",
+    listSelector: "div > article",
+    titleSelector: "h2 > a,h1 > a",
+    linkSelector: "h2 > a,h1 > a",
+  },
+  {
+    type: "web",
+    name: "The Rising Nepal",
+    url: "https://risingnepaldaily.com/categories/nation",
+    listSelector: ".item-content",
+    titleSelector: "h2 > a",
+    linkSelector: "h2 > a",
+  },
+
+  {
+    type: "web",
+    name: "Gorkapatra",
+    url: "https://gorkhapatraonline.com/categories/national",
+    listSelector: ".item-content",
+    titleSelector: "h2 > a",
+    linkSelector: "h2 > a",
+  },
+  {
+    type: "web",
+    name: "Aarthik News",
+    url: "https://aarthiknews.com",
+    baseUrl: "https://aarthiknews.com",
+    listSelector: ".top__stories-grid > .grid__card > .card__details",
+    titleSelector: "h3 > a",
+    linkSelector: "h3 > a",
   },
 ];
