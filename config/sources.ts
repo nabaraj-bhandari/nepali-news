2cc062a4e9775944095683f2844850aff4062034
nabaraj-bhandari <nabarajbhandari2005@gmail.com>
2025-09-24 10:49:16 +0545

    added site enable/disable toggle

--- config/sources.ts
+++ config/sources.ts
@@ -1,64 +1,6 @@
 import { WebScraperConfig } from "@/types/types";
 
-export const SITE_CONFIGS: WebScraperConfig[] = [
-  // RSS
-  {
-    type: "rss",
-    name: "OnlineKhabar",
-    url: "https://www.onlinekhabar.com/feed",
-  },
-  {
-    type: "rss",
-    name: "Rajdhani Daily",
-    url: "https://www.rajdhanidaily.com/feed",
-  },
-  {
-    type: "rss",
-    name: "News24 Nepal",
-    url: "https://www.news24nepal.com/feed",
-  },
-  {
-    type: "rss",
-    name: "OS Nepal",
-    url: "https://www.osnepal.com/feed",
-  },
-  {
-    type: "rss",
-    name: "Ratopati",
-    url: "https://www.ratopati.com/feed",
-  },
-  {
-    type: "rss",
-    name: "Setopati",
-    url: "https://www.setopati.com/feed",
-  },
-  {
-    type: "rss",
-    name: "Artha Sarokar",
-    url: "https://arthasarokar.com/feed",
-  },
-  {
-    type: "rss",
-    name: "Nepal Samaya",
-    url: "https://nepalsamaya.com/feed",
-  },
-  {
-    type: "rss",
-    name: "Nepali Patra",
-    url: "https://www.nepalipatra.com/feed",
-  },
-  {
-    type: "rss",
-    name: "१२खरी",
-    url: "https://baahrakhari.com/feed",
-  },
-  {
-    type: "rss",
-    name: "लोकान्तर",
-    url: "https://lokaantar.com/feed",
-  },
-
-  // Web
+export const WEB_SOURCES: WebScraperConfig[] = [
   {
     type: "web",
     name: "The Kathmandu Post",
@@ -67,6 +9,7 @@ export const SITE_CONFIGS: WebScraperConfig[] = [
     listSelector: "div > article",
     titleSelector: "a > h3",
     linkSelector: "a",
+    enabled: true,
   },
   {
     type: "web",
@@ -76,6 +19,7 @@ export const SITE_CONFIGS: WebScraperConfig[] = [
     listSelector: "article > .text",
     titleSelector: "h1 > a",
     linkSelector: "h1 > a",
+    enabled: true,
   },
   {
     type: "web",
@@ -84,6 +28,7 @@ export const SITE_CONFIGS: WebScraperConfig[] = [
     listSelector: "div > article",
     titleSelector: "h2 > a,h1 > a",
     linkSelector: "h2 > a,h1 > a",
+    enabled: true,
   },
   {
     type: "web",
@@ -92,6 +37,7 @@ export const SITE_CONFIGS: WebScraperConfig[] = [
     listSelector: ".item-content",
     titleSelector: "h2 > a",
     linkSelector: "h2 > a",
+    enabled: true,
   },
 
   {
@@ -101,6 +47,7 @@ export const SITE_CONFIGS: WebScraperConfig[] = [
     listSelector: ".item-content",
     titleSelector: "h2 > a",
     linkSelector: "h2 > a",
+    enabled: true,
   },
   {
     type: "web",
@@ -110,5 +57,80 @@ export const SITE_CONFIGS: WebScraperConfig[] = [
     listSelector: ".top__stories-grid > .grid__card > .card__details",
     titleSelector: "h3 > a",
     linkSelector: "h3 > a",
+    enabled: true,
   },
+];
+
+export const RSS_SOURCES: WebScraperConfig[] = [
+  {
+    type: "rss",
+    name: "OnlineKhabar",
+    url: "https://www.onlinekhabar.com/feed",
+    enabled: true,
+  },
+  {
+    type: "rss",
+    name: "Rajdhani Daily",
+    url: "https://www.rajdhanidaily.com/feed",
+    enabled: true,
+  },
+  {
+    type: "rss",
+    name: "News24 Nepal",
+    url: "https://www.news24nepal.com/feed",
+    enabled: true,
+  },
+  {
+    type: "rss",
+    name: "OS Nepal",
+    url: "https://www.osnepal.com/feed",
+    enabled: true,
+  },
+  {
+    type: "rss",
+    name: "Ratopati",
+    url: "https://www.ratopati.com/feed",
+    enabled: true,
+  },
+  {
+    type: "rss",
+    name: "Setopati",
+    url: "https://www.setopati.com/feed",
+    enabled: true,
+  },
+  {
+    type: "rss",
+    name: "Artha Sarokar",
+    url: "https://arthasarokar.com/feed",
+    enabled: true,
+  },
+  {
+    type: "rss",
+    name: "Nepal Samaya",
+    url: "https://nepalsamaya.com/feed",
+    enabled: true,
+  },
+  {
+    type: "rss",
+    name: "Nepali Patra",
+    url: "https://www.nepalipatra.com/feed",
+    enabled: true,
+  },
+  {
+    type: "rss",
+    name: "१२खरी",
+    url: "https://baahrakhari.com/feed",
+    enabled: true,
+  },
+  {
+    type: "rss",
+    name: "लोकान्तर",
+    url: "https://lokaantar.com/feed",
+    enabled: true,
+  },
+];
+
+export const SITE_CONFIGS: WebScraperConfig[] = [
+  ...RSS_SOURCES.filter((site) => site.enabled),
+  ...WEB_SOURCES.filter((site) => site.enabled),
 ];
--- types/types.ts
+++ types/types.ts
@@ -4,12 +4,22 @@ export interface News {
   source: string;
 }
 
-export type WebScraperConfig = {
-  type: "rss" | "web";
+export interface RSSConfig {
+  type: "rss";
+  name: string;
+  url: string;
+  enabled?: boolean;
+}
+
+export interface WebConfig {
+  type: "web";
   name: string;
   url: string;
   baseUrl?: string;
-  listSelector?: string;
-  titleSelector?: string;
-  linkSelector?: string;
-};
+  listSelector: string;
+  titleSelector: string;
+  linkSelector: string;
+  enabled?: boolean;
+}
+
+export type WebScraperConfig = RSSConfig | WebConfig;
